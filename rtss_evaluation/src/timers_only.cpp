0ce50c392100b21bca58c49ec0527d1124d036c0
Oren Bell <oren.bell@wustl.edu>
2024-05-15 01:29:41 +0000

    Modifications for evaluations
    Signed-off-by: Oren Bell <oren.bell@wustl.edu>

--- rtss_evaluation/src/timers_only.cpp
+++ rtss_evaluation/src/timers_only.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <thread>
+
 #include "rclcpp/rclcpp.hpp"
 
 #include "rtss_evaluation/rt_system.hpp"
@@ -24,8 +26,8 @@
 int main(int argc, char * argv[])
 {
   // Check for correct number of arguments
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <executor type> [RO|RE]" << std::endl;
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << "<duration <executor type> [RO|RE]" << std::endl;
     return 1;
   }
 
@@ -39,10 +41,25 @@ int main(int argc, char * argv[])
   // Build a set of timers
   auto nodes = create_timer_only_nodes<RTSystem, TimeConfig>();
 
-  // Using argv[3] as a string argument, ascertain which executor we're running
-  if (strcmp(argv[1], "rm") == 0) {
-    std::cout << "Using rate monotonic fixed priority executor" << std::endl;
-    // TODO: Get argv[4] to determine RO or RE
+  int duration = std::stoi(argv[1]);
+
+  // Create a new thread that sleeps for the specified duration
+  std::thread shutdown_thread([duration]() {
+    std::this_thread::sleep_for(std::chrono::seconds(duration));
+    rclcpp::shutdown();
+  });
+
+  // Using argv[2] as a string argument, ascertain which executor we're running
+  if (strcmp(argv[2], "rm") == 0) {
+
+    // Get argv[4] to determine RO or RE
+    bool execute_timers_separate_thread = false;
+    if (argc >= 3 && strcmp(argv[3], "RE") == 0) {
+      execute_timers_separate_thread = true;
+      std::cout << "Using rate monotonic fixed priority executor in RE mode" << std::endl;
+    } else {
+      std::cout << "Using rate monotonic fixed priority executor in RO mode" << std::endl;
+    }
 
     rclcpp::experimental::executors::GraphExecutor executor = rclcpp::experimental::executors::GraphExecutor();
 
@@ -51,13 +68,19 @@ int main(int argc, char * argv[])
     }
     executor.assign_priority();
     executor.spin();
-  } else if (strcmp(argv[1], "events") == 0) {
-    std::cout << "Using events executor" << std::endl;
+  } else if (strcmp(argv[2], "events") == 0) {
 
-    // TODO: Get argv[4] to determine RO or RE
+    // Get argv[4] to determine RO or RE
+    bool execute_timers_separate_thread = false;
+    if (argc >= 3 && strcmp(argv[3], "RE") == 0) {
+      execute_timers_separate_thread = true;
+      std::cout << "Using events executor in RE mode" << std::endl;
+    } else {
+      std::cout << "Using events executor in RO mode" << std::endl;
+    }
 
     auto events_queue = std::make_unique<rclcpp::experimental::executors::PriorityEventsQueue>();
-    rclcpp::experimental::executors::EventsExecutor executor(std::move(events_queue), false);
+    rclcpp::experimental::executors::EventsExecutor executor(std::move(events_queue), execute_timers_separate_thread);
     for (auto & node : nodes) {
       executor.add_node(node);
     }
@@ -73,10 +96,20 @@ int main(int argc, char * argv[])
     executor.spin();
   }
 
+  // Wait for the thread to finish
+  shutdown_thread.join();
+
+  uint32_t dropped_jobs_sum = 0;
+  uint32_t total_jobs_sum = 0;
   std::cout << "Dropped jobs: " << std::endl;
   for (auto & node : nodes) {
-    std::cout << node->get_name() << ": " << node->get_dropped_jobs() << std::endl;
+    uint32_t dropped_jobs = node->get_dropped_jobs();
+    uint32_t total_jobs = node->get_completed_jobs() + node->get_dropped_jobs();
+    std::cout << node->get_name() << ": " << dropped_jobs << " / " << total_jobs << std::endl;
+    dropped_jobs_sum += dropped_jobs;
+    total_jobs_sum += total_jobs;
   }
+  std::cout << "Total: " << dropped_jobs_sum << " / " << total_jobs_sum << std::endl;
 
   nodes.clear();
   rclcpp::shutdown();
